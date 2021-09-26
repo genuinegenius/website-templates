4891a335f9b2a767300a9f48b9fd9dd1ac1bc6b0
genuinegenius <76172880+genuinegenius@users.noreply.github.com>
2021-09-27 00:54:02 +0300

    Update script.js
    small adjustments on page printing of elements from menu_left in js which were stored in local storage

--- script.js
+++ script.js
@@ -18,11 +18,13 @@ if (!(auxForLocalStorage == menuState)) {
 
 let menuLeftBar = id('menu_left');
 
-
 //print menu_left options from localstorage
 menuState.forEach(e => {
-    menuLeftBar.innerHTML = menuLeftBar.innerHTML + '<button id="' + e + '" class="buttons_menu" buttons_menu>' + e + '</button>';
+    menuLeftBar.innerHTML +=
+        '<button id="' + e + '" class="buttons_menu" buttons_menu>' + e + '</button>';
 })
+
+menuLeftBar.innerHTML += '<div id="button_menu_add_container" class="button_menu_add_container" button_menu_add_container><button id="button_menu_add" class="button_menu_add" button_menu_add>&#10010;</button><div id="menu_add_content" class="menu_add_content"></div></div>';
 
 document.addEventListener("click", e => {
     let button_add = e.target.matches('[button_menu_add]');
